7af53d0d953b5fa071d300fa06f4be844a21e607
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-10-09 21:04:11 +0300

    feat: first task solution

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,25 @@
 import {encoded, translations} from './data.js'
 
-console.log("Let's rock")
-console.log(encoded, translations)
+// console.log("Let's rock")
+// console.log(encoded, translations)
+
+
+function decoded(encoded, translations) {
+  const set = new Set()
+  const arr = encoded.map(i => {
+    const obj = {}
+    Object.keys(i).forEach((prop) => {
+      if(prop === 'groupId') set.add(i[prop])
+      if(prop.split('Id') && prop !== 'groupId') obj[prop] =  translations[i[prop]] || i[prop]
+      else obj[prop] = i[prop]
+      if(i[prop] === null) obj[prop] = null
+    })
+    return obj
+  })
+  arr.push(set.keys())
+  return arr
+}
 
 
 
-// console.log(decoded)
+console.log(decoded(encoded, translations))
